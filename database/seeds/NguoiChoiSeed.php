0b4645daee4b5ccb353d2a2567ddda8d6eb07792
tranvominhhuy <55388334+tranvominhhuy@users.noreply.github.com>
2019-12-28 16:34:36 +0700

    update nguoi choi seeder

--- database/seeds/NguoiChoiSeed.php
+++ database/seeds/NguoiChoiSeed.php
@@ -21,5 +21,15 @@ class NguoiChoiSeed extends Seeder
         	'credit'			=> 123
 
         ]);
+        App\NguoiChoi::create([
+
+            'ten_dang_nhap'     => 'huy',
+            'email'             => 'huy',
+            'mat_khau'          => Hash::make('123456'),
+            'hinh_dai_dien'     => '',
+            'diem_cao_nhat'     => 12345,
+            'credit'            => 12345
+
+        ]);
     }
 }
